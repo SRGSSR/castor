4b9598041a91fd72492967cfd272eb2890c264bd
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-01-28 11:15:03 +0100

    Add demo app

---
+++ Demo/Sources/DemoApp.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
