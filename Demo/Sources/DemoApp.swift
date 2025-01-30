2f8182e751dc907688566e4bce6631b12253f159
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-01-30 15:10:02 +0100

    Set up audio session

--- Demo/Sources/DemoApp.swift
+++ Demo/Sources/DemoApp.swift
@@ -4,10 +4,21 @@
 //  License information is available from the LICENSE file.
 //
 
+import AVFAudio
 import SwiftUI
+
+private final class AppDelegate: NSObject, UIApplicationDelegate {
+    // swiftlint:disable:next discouraged_optional_collection
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+        return true
+    }
+}
 
 @main
 struct DemoApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     var body: some Scene {
         WindowGroup {
             NavigationStack {
