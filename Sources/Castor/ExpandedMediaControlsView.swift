67795ae7a80fe01229443c224cef3687d7a07a10
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-01-31 13:49:51 +0100

    Provide an expanded media controls view

---
+++ Sources/Castor/ExpandedMediaControlsView.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+import SwiftUI
+
+private struct _ExpandedMediaControlsView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> GCKUIExpandedMediaControlsViewController {
+        GCKCastContext.sharedInstance().defaultExpandedMediaControlsViewController
+    }
+
+    func updateUIViewController(_ uiViewController: GCKUIExpandedMediaControlsViewController, context: Context) {}
+}
+
+/// An expanded media controls view.
+public struct ExpandedMediaControlsView: View {
+    public var body: some View {
+        _ExpandedMediaControlsView()
+            .ignoresSafeArea()
+    }
+
+    public init() {}
+}
