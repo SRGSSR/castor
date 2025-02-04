efc13dff988c6aebdeaf52d315b58df121cec294
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-02-04 11:00:57 +0100

    Introduce CastDeviceManager

---
+++ Sources/Castor/CastDeviceManager.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+import GoogleCast
+
+public final class CastDeviceManager: NSObject, ObservableObject {
+    private let context = GCKCastContext.sharedInstance()
+
+    @Published public private(set) var devices: [GCKDevice]
+
+    override public init() {
+        devices = Self.devices(from: context.discoveryManager)
+        super.init()
+        context.discoveryManager.add(self)
+    }
+}
+
+extension CastDeviceManager: GCKDiscoveryManagerListener {
+    public func didInsert(_ device: GCKDevice, at index: UInt) {
+        devices.insert(device, at: Int(index))
+    }
+
+    public func didRemove(_ device: GCKDevice, at index: UInt) {
+        devices.remove(at: Int(index))
+    }
+}
+
+private extension CastDeviceManager {
+    static func devices(from discoveryManager: GCKDiscoveryManager) -> [GCKDevice] {
+        var devices: [GCKDevice] = []
+        for index in 0..<discoveryManager.deviceCount {
+            devices.append(discoveryManager.device(at: index))
+        }
+        return devices
+    }
+}
