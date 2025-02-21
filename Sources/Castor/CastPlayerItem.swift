4058108fad6d6aa2669e01925e12062576815130
Samuel Défago <defagos@gmail.com>
2025-02-21 17:36:26 +0100

    Prepare item placeholder support

--- Sources/Castor/CastPlayerItem.swift
+++ Sources/Castor/CastPlayerItem.swift
@@ -8,25 +8,36 @@ import GoogleCast
 
 /// A cast player item.
 public struct CastPlayerItem: Hashable {
-    let rawItem: GCKMediaQueueItem
+    let id: GCKMediaQueueItemID
+    let rawItem: GCKMediaQueueItem?
 
     /// The content title.
     public var title: String? {
-        rawItem.mediaInformation.metadata?.string(forKey: kGCKMetadataKeyTitle)
+        rawItem?.mediaInformation.metadata?.string(forKey: kGCKMetadataKeyTitle)
     }
 
-    fileprivate init(rawItem: GCKMediaQueueItem) {
+    /// Return `true` if item metadata has been loaded
+    public var isLoaded: Bool {
+        rawItem != nil
+    }
+
+    fileprivate init(id: GCKMediaQueueItemID, rawItem: GCKMediaQueueItem?) {
+        self.id = id
         self.rawItem = rawItem
+    }
+
+    static func loadingItem(withId id: GCKMediaQueueItemID) -> Self {
+        .init(id: id, rawItem: nil)
     }
 
     // swiftlint:disable:next missing_docs
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.rawItem.itemID == rhs.rawItem.itemID
+        lhs.id == rhs.id
     }
 }
 
 extension GCKMediaQueueItem {
     func toCastPlayerItem() -> CastPlayerItem {
-        .init(rawItem: self)
+        .init(id: itemID, rawItem: self)
     }
 }
