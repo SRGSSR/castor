1624655699f22c164877de206a3af66221c30b3b
Samuel Défago <defagos@gmail.com>
2025-02-27 13:17:50 +0100

    Prepare playlist mutation API

--- Sources/Castor/MediaQueue.swift
+++ Sources/Castor/MediaQueue.swift
@@ -53,6 +53,124 @@ public final class MediaQueue: NSObject, ObservableObject {
             self.currentItem = newValue
         }
     }
+
+    /// Inserts an item before another one.
+    ///
+    /// - Parameters:
+    ///   - item: The item to insert.
+    ///   - beforeItem: The item before which insertion must take place. Pass `nil` to insert the item at the front
+    ///     of the queue.
+    /// - Returns: `true` iff the item could be inserted.
+    @discardableResult
+    public func insert(_ item: CastPlayerItem, before beforeItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Insert items before another one.
+    ///
+    /// - Parameters:
+    ///   - items: The items to insert.
+    ///   - beforeItem: The item before which insertion must take place. Pass `nil` to insert the items at the front
+    ///     of the queue.
+    /// - Returns: `true` iff the items could be inserted.
+    @discardableResult
+    public func insert(_ items: [CastPlayerItem], before beforeItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Inserts an item after another one.
+    ///
+    /// - Parameters:
+    ///   - item: The item to insert.
+    ///   - afterItem: The item after which insertion must take place. Pass `nil` to insert the item at the back of
+    ///     the queue. If this item does not exist the method does nothing.
+    /// - Returns: `true` iff the item could be inserted.
+    @discardableResult
+    public func insert(_ item: CastPlayerItem, after afterItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Inserts items after another one.
+    ///
+    /// - Parameters:
+    ///   - items: The items to insert.
+    ///   - afterItem: The item after which insertion must take place. Pass `nil` to insert the items at the back of
+    ///     the queue. If this item does not exist the method does nothing.
+    /// - Returns: `true` iff the items could be inserted.
+    @discardableResult
+    public func insert(_ items: [CastPlayerItem], after afterItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Prepends an item to the queue.
+    ///
+    /// - Parameter item: The item to prepend.
+    /// - Returns: `true` iff the item could be prepended.
+    @discardableResult
+    func prepend(_ item: CastPlayerItem) -> Bool {
+        false
+    }
+
+    /// Prepends items to the queue.
+    ///
+    /// - Parameter items: The items to prepend.
+    /// - Returns: `true` iff the items could be prepended.
+    @discardableResult
+    func prepend(_ items: [CastPlayerItem]) -> Bool {
+        false
+    }
+
+    /// Appends an item to the queue.
+    ///
+    /// - Parameter item: The item to append.
+    /// - Returns: `true` iff the item could be appended.
+    @discardableResult
+    func append(_ item: CastPlayerItem) -> Bool {
+        false
+    }
+
+    /// Appends items to the queue.
+    ///
+    /// - Parameter items: The items to append.
+    /// - Returns: `true` iff the items could be appended.
+    @discardableResult
+    func append(_ items: [CastPlayerItem]) -> Bool {
+        false
+    }
+
+    /// Moves an item before another one.
+    ///
+    /// - Parameters:
+    ///   - item: The item to move. The method does nothing if the item does not belong to the queue.
+    ///   - beforeItem: The item before which the moved item must be relocated. Pass `nil` to move the item to the
+    ///     front of the queue. If the item does not belong to the queue the method does nothing.
+    /// - Returns: `true` iff the item could be moved.
+    @discardableResult
+    func move(_ item: CastPlayerItem, before beforeItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Moves an item after another one.
+    ///
+    /// - Parameters:
+    ///   - item: The item to move.
+    ///   - afterItem: The item after which the moved item must be relocated. Pass `nil` to move the item to the
+    ///     back of the queue. If the item does not belong to the queue the method does nothing.
+    /// - Returns: `true` iff the item could be moved.
+    @discardableResult
+    func move(_ item: CastPlayerItem, after afterItem: CastPlayerItem?) -> Bool {
+        false
+    }
+
+    /// Removes an item from the queue.
+    ///
+    /// - Parameter item: The item to remove.
+    func remove(_ item: CastPlayerItem) {
+    }
+
+    /// Removes all items from the queue.
+    func removeAllItems() {
+    }
 }
 
 extension MediaQueue: GCKRemoteMediaClientListener {
