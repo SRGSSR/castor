b33da58bafc21238757ded6f6dc76e6b498eabc9
Samuel Défago <defagos@gmail.com>
2025-03-06 15:51:11 +0100

    Fix item identity and hashability

--- Sources/Castor/CastPlayerItem.swift
+++ Sources/Castor/CastPlayerItem.swift
@@ -9,15 +9,12 @@ import GoogleCast
 /// A cast player item.
 public struct CastPlayerItem: Hashable {
     let id: GCKMediaQueueItemID
-    let rawItem: GCKMediaQueueItem?
 
     /// The content title.
-    public var title: String? {
-        rawItem?.mediaInformation.metadata?.string(forKey: kGCKMetadataKeyTitle)
-    }
+    public let title: String?
 
-    // swiftlint:disable:next missing_docs
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id && lhs.rawItem?.itemID == rhs.rawItem?.itemID
+    init(id: GCKMediaQueueItemID, rawItem: GCKMediaQueueItem?) {
+        self.id = id
+        self.title = rawItem?.mediaInformation.metadata?.string(forKey: kGCKMetadataKeyTitle)
     }
 }
