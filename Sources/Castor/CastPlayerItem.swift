747ced9e60d3ec5348cf9edb7b6386a41abf3948
Samuel Défago <defagos@gmail.com>
2025-03-26 11:11:35 +0100

    Cache items

--- Sources/Castor/CastPlayerItem.swift
+++ Sources/Castor/CastPlayerItem.swift
@@ -20,13 +20,13 @@ public struct CastPlayerItem: Hashable {
         self.id = id
     }
 
-    /// Asynchronously fetches item metadata from the specified queue.
+    /// Asynchronously fetches an item from the specified queue.
     ///
     /// - Parameter queue: The queue to retrieve metadata from.
     ///
     /// Fetching an item from a queue to which it does not belong leads to unexpected behavior.
-    public func fetchMetadata(from queue: CastQueue) {
-        queue.fetchMetadata(for: self)
+    public func fetch(from queue: CastQueue) {
+        queue.fetch(self)
     }
 
     /// Reads item metadata from the specified queue.
@@ -36,6 +36,7 @@ public struct CastPlayerItem: Hashable {
     /// Reading metadata for an item from a queue to which it does not belong leads to unexpected behavior. Metadata
     /// must be fetched first by calling ``fetchMetadata(from:)``.
     public func metadata(from queue: CastQueue) -> CastMetadata? {
-        queue.metadata(for: self)
+        guard let rawItem = queue.rawItem(for: self) else { return nil }
+        return .init(rawMetadata: rawItem.mediaInformation.metadata)
     }
 }
