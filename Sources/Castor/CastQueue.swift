cf0fdad8290d8854584b486ebc0734c495fc2628
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-03-28 09:24:25 +0100

    Add APIs to insert after an item

--- Sources/Castor/CastQueue.swift
+++ Sources/Castor/CastQueue.swift
@@ -116,7 +116,7 @@ public extension CastQueue {
     ///
     /// - Parameters:
     ///   - assets: The assets for the items to insert.
-    ///   - beforeItem: The item before which insertion must take place. Pass `nil` to insert the item at the front
+    ///   - beforeItem: The item before which insertion must take place. Pass `nil` to insert the items at the front
     ///     of the queue.
     @discardableResult
     func insertItems(from assets: [CastAsset], before beforeItem: CastPlayerItem?) -> Bool {
@@ -125,9 +125,39 @@ public extension CastQueue {
             remoteMediaClient.queueInsert(Self.rawItems(from: assets), beforeItemWithID: beforeItem.id)
         }
         else {
+            prependItems(from: assets)
+        }
+        return true
+    }
+
+    /// Inserts items after another one.
+    ///
+    /// - Parameters:
+    ///   - assets: The assets for the items to insert.
+    ///   - afterItem: The item after which insertion must take place. Pass `nil` to insert the items at the back
+    ///     of the queue.
+    @discardableResult
+    func insertItems(from assets: [CastAsset], after afterItem: CastPlayerItem?) -> Bool {
+        if let afterItem {
+            guard let afterItemIndex = items.firstIndex(of: afterItem) else { return false }
+            let nextItem = items[safeIndex: items.index(after: afterItemIndex)]
+            remoteMediaClient.queueInsert(Self.rawItems(from: assets), beforeItemWithID: nextItem?.id ?? kGCKMediaQueueInvalidItemID)
+        }
+        else {
             appendItems(from: assets)
         }
         return true
+    }
+
+    /// Inserts an item after another one.
+    ///
+    /// - Parameters:
+    ///   - asset: The asset for the item to insert.
+    ///   - afterItem: The item after which insertion must take place. Pass `nil` to insert the item at the back
+    ///     of the queue.
+    @discardableResult
+    func insertItem(from asset: CastAsset, after afterItem: CastPlayerItem?) -> Bool {
+        insertItems(from: [asset], after: afterItem)
     }
 
     /// Insert an item before another one.
@@ -162,7 +192,7 @@ public extension CastQueue {
 
     /// Prepends items to the queue.
     ///
-    /// - Parameter assets: The assets for the item to insert.
+    /// - Parameter assets: The assets for the items to prepend.
     func prependItems(from assets: [CastAsset]) {
         if let firstItem = items.first {
             remoteMediaClient.queueInsert(Self.rawItems(from: assets), beforeItemWithID: firstItem.id)
@@ -174,7 +204,7 @@ public extension CastQueue {
 
     /// Prepends an item to the queue.
     ///
-    /// - Parameter asset: The asset for the item to insert.
+    /// - Parameter asset: The asset for the item to prepend.
     func prependItem(from asset: CastAsset) {
         prependItems(from: [asset])
     }
