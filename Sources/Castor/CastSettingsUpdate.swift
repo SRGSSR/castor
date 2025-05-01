b892e1f29b7dc3fa6adea120fa96210abeb7c8ad
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-05-01 15:42:44 +0200

    Introduce CastSettingsUpdate

---
+++ Sources/Castor/CastSettingsUpdate.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+/// A settings update.
+public enum CastSettingsUpdate {
+    /// Playback speed.
+    case playbackSpeed(Float)
+}
