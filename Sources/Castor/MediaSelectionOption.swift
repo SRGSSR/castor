9945a551dbceaa9755d002567b97defdd98240b3
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-05-06 15:27:02 +0200

    Update localizable strings

--- Sources/Castor/MediaSelectionOption.swift
+++ Sources/Castor/MediaSelectionOption.swift
@@ -23,7 +23,7 @@ public enum CastMediaSelectionOption: Hashable {
     public var displayName: String {
         switch self {
         case .off:
-            return String(localized: "Off", bundle: .module, comment: "Subtitle selection option")
+            return String(localized: "Off", bundle: .module, comment: "Media selection option")
         case let .on(option):
             return option.displayName
         }
