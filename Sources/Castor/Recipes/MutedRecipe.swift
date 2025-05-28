f0a1c962ed0e0ad1e5a5454e9905507a2f744d52
Samuel Défago <defagos@gmail.com>
2025-05-28 20:45:18 +0200

    Split code over several files

---
+++ Sources/Castor/Recipes/MutedRecipe.swift
@@ -0,0 +1,38 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+
+final class MutedRecipe: NSObject, SynchronizerRecipe, GCKSessionManagerListener {
+    static let defaultValue = false
+
+    let service: GCKSessionManager
+    private let update: (DeviceSettings?) -> Void
+
+    init(service: GCKSessionManager, update: @escaping (DeviceSettings?) -> Void) {
+        self.service = service
+        self.update = update
+        super.init()
+        service.add(self)
+    }
+
+    func value(from status: DeviceSettings) -> Bool {
+        status.isMuted
+    }
+
+    func makeRequest(for value: Bool, using requester: GCKCastSession) -> GCKRequest {
+        requester.setDeviceMuted(value)
+    }
+
+    func sessionManager(
+        _ sessionManager: GCKSessionManager,
+        castSession session: GCKCastSession,
+        didReceiveDeviceVolume volume: Float,
+        muted: Bool
+    ) {
+        update(.init(volume: volume, isMuted: muted))
+    }
+}
