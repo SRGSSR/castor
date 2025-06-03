31a6a1469bb43291d332b6cc55d97691ec8d82ce
Samuel Défago <defagos@gmail.com>
2025-06-03 19:08:43 +0200

    Create lazy item

---
+++ Sources/Castor/LazyItem.swift
@@ -0,0 +1,66 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+import GoogleCast
+
+@propertyWrapper
+final class LazyItemPropertyWrapper<Instance>: NSObject, GCKMediaQueueDelegate where Instance: ObservableObject, Instance.ObjectWillChangePublisher == ObservableObjectPublisher {
+    private let id: GCKMediaQueueItemID
+    private let queue: GCKMediaQueue
+    private weak var enclosingInstance: Instance?
+
+    @available(*, unavailable, message: "@LazyItem can only be applied to classes")
+    var wrappedValue: GCKMediaQueueItem? {
+        fatalError("Not available")
+    }
+
+    @Published private var value: GCKMediaQueueItem? {
+        willSet {
+            enclosingInstance?.objectWillChange.send()
+        }
+    }
+
+    init(id: GCKMediaQueueItemID, queue: GCKMediaQueue) {
+        self.id = id
+        self.queue = queue
+        super.init()
+        queue.add(self)
+    }
+
+    static subscript(
+        _enclosingInstance instance: Instance,
+        wrapped wrappedKeyPath: KeyPath<Instance, GCKMediaQueueItem?>,
+        storage storageKeyPath: KeyPath<Instance, LazyItemPropertyWrapper>
+    ) -> GCKMediaQueueItem? {
+        get {
+            let wrapper = instance[keyPath: storageKeyPath]
+            wrapper.enclosingInstance = instance
+            return wrapper.value
+        }
+        set {}
+    }
+
+    func fetch() {
+        guard value == nil else { return }
+        if let item = queue.item(withID: id, fetchIfNeeded: false) {
+            value = item
+        }
+        else {
+            queue.item(withID: id)
+        }
+    }
+
+    func mediaQueue(_ queue: GCKMediaQueue, didUpdateItemsAtIndexes indexes: [NSNumber]) {
+        if let item = queue.item(withID: id, fetchIfNeeded: false) {
+            value = item
+        }
+    }
+}
+
+extension ObservableObject where ObjectWillChangePublisher == ObservableObjectPublisher {
+    typealias LazyItems = LazyItemPropertyWrapper<Self>
+}
