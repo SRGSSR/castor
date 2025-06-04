7c8512cfaea563900204a6929f90051350c2914e
Samuel Défago <defagos@gmail.com>
2025-06-04 13:01:54 +0200

    Avoid redundant recipe logic betwen mutating and non-mutating variants

--- Sources/Castor/Protocols/SynchronizerRecipe.swift
+++ Sources/Castor/Protocols/SynchronizerRecipe.swift
@@ -6,41 +6,6 @@
 
 import GoogleCast
 
-protocol SynchronizerRecipe: AnyObject {
-    associatedtype Service
-
-    associatedtype Status
-    associatedtype Value: Equatable
-
-    static var defaultValue: Value { get }
-
-    var service: Service { get }
-
+protocol SynchronizerRecipe: BaseSynchronizerRecipe {
     init(service: Service, update: @escaping (Status?) -> Void)
-
-    static func status(from service: Service) -> Status?
-    static func value(from status: Status) -> Value
-}
-
-extension SynchronizerRecipe where Status == Value {
-    static func value(from status: Status) -> Value {
-        status
-    }
-}
-
-extension SynchronizerRecipe where Optional<Status> == Value {
-    static func value(from status: Status) -> Value {
-        status
-    }
-}
-
-extension SynchronizerRecipe {
-    func value(from service: Service, defaultValue: Value) -> Value {
-        value(from: Self.status(from: service), defaultValue: defaultValue)
-    }
-
-    func value(from status: Status?, defaultValue: Value) -> Value {
-        guard let status else { return defaultValue }
-        return Self.value(from: status)
-    }
 }
