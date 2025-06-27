2818db276b961ac1dbde0abb77ff64d19d6a5891
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-06-27 11:01:35 +0200

    Introduce CastResumeState

---
+++ Sources/Castor/Types/CastResumeState.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import CoreMedia
+
+/// State information useful when transitioning between remote and local playback.
+public struct CastResumeState {
+    /// The assets.
+    public let assets: [CastAsset]
+
+    /// The current index.
+    public let index: Int
+
+    /// The current time.
+    public let time: CMTime
+
+    /// Creates a state.
+    /// - Parameters:
+    ///   - assets: The assets.
+    ///   - index: The current index.
+    ///   - time: The current time.
+    public init?(assets: [CastAsset], index: Int, time: CMTime) {
+        guard index < assets.endIndex else { return nil }
+        self.assets = assets
+        self.index = index
+        self.time = time
+    }
+}
