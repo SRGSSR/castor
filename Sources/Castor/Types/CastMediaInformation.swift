2eb264444e4fbe29a42f54aff7f601d459c37c7d
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-06-27 12:41:38 +0200

    Introduce CastMediaInformation

---
+++ Sources/Castor/Types/CastMediaInformation.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+
+/// The media information useful for casting.
+public struct CastMediaInformation {
+    let rawMediaInformation: GCKMediaInformation
+
+    /// The metadata.
+    public let metadata: CastMetadata
+
+    /// The URL of the content.
+    public var contentUrl: URL? {
+        rawMediaInformation.contentURL
+    }
+
+    /// The content identifier.
+    public var contentIdentifier: String? {
+        rawMediaInformation.contentID
+    }
+
+    init(rawMediaInformation: GCKMediaInformation) {
+        self.rawMediaInformation = rawMediaInformation
+        self.metadata = .init(rawMetadata: rawMediaInformation.metadata)
+    }
+}
