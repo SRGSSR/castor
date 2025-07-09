62a24431f1584920c5f3b013fb0ddb913fbb01ce
Samuel Défago <defagos@gmail.com>
2025-07-09 15:40:35 +0200

    Use shorthand syntax

--- Demo/Sources/Router.swift
+++ Demo/Sources/Router.swift
@@ -40,7 +40,7 @@ extension Router: CastDelegate {
     }
 
     func castEndSession(with state: CastResumeState) {
-        let medias = state.assets.map { Media(from: $0) }
+        let medias = state.assets.map(Media.init)
         let time = state.time.isValid ? state.time : .zero
         presented = .player(content: .init(medias: medias, startIndex: state.index, startTime: time))
     }
