e5c1738bef37d6f119813f0890e74b0c8d63b1c7
Samuel Défago <defagos@gmail.com>
2025-07-11 08:48:03 +0200

    Remove code distracting from the purpose of a demo

--- Demo/Sources/Extensions/View.swift
+++ Demo/Sources/Extensions/View.swift
@@ -24,77 +24,7 @@ extension View {
         modifier(PulseSymbolEffect17())
     }
 
-    @ViewBuilder
-    func toolbarBackgroundVisibilityForTabBar18(_ visibility: Visibility) -> some View {
-        if #available(iOS 18.0, *) {
-            toolbarBackgroundVisibility(visibility, for: .tabBar)
-        }
-        else {
-            self
-        }
-    }
-
     func redacted(_ condition: Bool) -> some View {
         redacted(reason: condition ? .placeholder : .init())
-    }
-}
-
-extension View {
-    @ViewBuilder
-    func tabViewBottomAccessoryMiniPlayer26(for cast: Cast, in namespace: Namespace.ID) -> some View {
-#if swift(>=6.2)
-        if #available(iOS 26, *) {
-            tabViewBottomAccessory {
-                MiniPlayerView(cast: cast)
-                    .matchedTransitionSource(id: TransitionId.zoom, in: namespace)
-            }
-            .tabBarMinimizeBehavior(.onScrollDown)
-        }
-        else {
-            self
-        }
-#else
-        self
-#endif
-    }
-
-    @ViewBuilder
-    func safeAreaInsetMiniPlayer(for cast: Cast, in namespace: Namespace.ID) -> some View {
-        if #unavailable(iOS 26) {
-            safeAreaInset(edge: .bottom, spacing: 0) {
-                if cast.player != nil {
-                    MiniPlayerView(cast: cast)
-                        .matchedTransitionSource18(id: TransitionId.zoom, in: namespace)
-                        .background(.thickMaterial)
-                        .frame(height: 64)
-                }
-            }
-            .toolbarBackgroundVisibilityForTabBar18(cast.player == nil ? .automatic : .hidden)
-        }
-        else {
-            self
-        }
-    }
-}
-
-extension View {
-    @ViewBuilder
-    func matchedTransitionSource18(id: some Hashable, in namespace: Namespace.ID) -> some View {
-        if #available(iOS 18, *) {
-            matchedTransitionSource(id: id, in: namespace)
-        }
-        else {
-            self
-        }
-    }
-
-    @ViewBuilder
-    func zoomNavigationTransition18(sourceID: some Hashable, in namespace: Namespace.ID) -> some View {
-        if #available(iOS 18, *) {
-            navigationTransition(.zoom(sourceID: sourceID, in: namespace))
-        }
-        else {
-            self
-        }
     }
 }
--- Demo/Sources/RootView.swift
+++ Demo/Sources/RootView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct RootView: View {
     @StateObject private var cast = Cast(configuration: .standard)
     @StateObject private var router = Router()
-    @Namespace private var namespace
 
     var body: some View {
         TabView {
             NavigationStack {
                 ExamplesView()
-                    .safeAreaInsetMiniPlayer(for: cast, in: namespace)
+                    .safeAreaInset(edge: .bottom, spacing: 0) {
+                        miniPlayer()
+                    }
             }
             .tabItem {
                 Label("Examples", systemImage: "film.fill")
@@ -36,13 +37,20 @@ struct RootView: View {
         }
         .sheet(item: $router.presented) { destination in
             destination.view()
-                .zoomNavigationTransition18(sourceID: TransitionId.zoom, in: namespace)
         }
-        .tabViewBottomAccessoryMiniPlayer26(for: cast, in: namespace)
         // TODO: Starting with iOS 17 this can be moved on the `DemoApp` window group.
         .environmentObject(cast)
         .environmentObject(router)
         .supportsCast(cast, with: router)
+    }
+
+    @ViewBuilder
+    private func miniPlayer() -> some View {
+        if cast.player != nil {
+            MiniPlayerView(cast: cast)
+                .background(.thickMaterial)
+                .frame(height: 64)
+        }
     }
 }
 
