b480406e5e901fba0572fb270ccd6f547017dfc6
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-08-04 14:59:16 +0200

    Improve tests

--- Tests/CastorTests/FormatStyleTests.swift
+++ Tests/CastorTests/FormatStyleTests.swift
@@ -11,7 +11,8 @@ struct FormatStyleTests {
     @Test
     func player_time_short_format_style() async throws {
         let formatter = PlayerTimeShortFormatStyle()
-        #expect(formatter.format(.init(value: 1, timescale: 0)).isEmpty)
+        #expect(formatter.format(.invalid).isEmpty)
+        #expect(formatter.format(.zero) == "00:00")
         #expect(formatter.format(.init(value: 1, timescale: 1)) == "00:01")
         #expect(formatter.format(.init(value: 60, timescale: 1)) == "01:00")
         #expect(formatter.format(.init(value: 3600, timescale: 1)) == "60:00")
@@ -20,7 +21,8 @@ struct FormatStyleTests {
     @Test
     func player_time_long_format_style() async throws {
         let formatter = PlayerTimeLongFormatStyle()
-        #expect(formatter.format(.init(value: 1, timescale: 0)).isEmpty)
+        #expect(formatter.format(.invalid).isEmpty)
+        #expect(formatter.format(.zero) == "00:00:00")
         #expect(formatter.format(.init(value: 1, timescale: 1)) == "00:00:01")
         #expect(formatter.format(.init(value: 60, timescale: 1)) == "00:01:00")
         #expect(formatter.format(.init(value: 3600, timescale: 1)) == "01:00:00")
@@ -29,15 +31,20 @@ struct FormatStyleTests {
     @Test
     func player_time_adaptive_format_style() async throws {
         var formatter = PlayerTimeAdaptiveFormatStyle(duration: 0)
+        #expect(formatter.format(.invalid).isEmpty)
         #expect(formatter.format(.zero).isEmpty)
-        #expect(formatter.format(.init(value: 0, timescale: 1)).isEmpty)
+        #expect(formatter.format(.init(value: 1, timescale: 1)).isEmpty)
 
         formatter = PlayerTimeAdaptiveFormatStyle(duration: 3599)
+        #expect(formatter.format(.invalid).isEmpty)
+        #expect(formatter.format(.zero) == "00:00")
         #expect(formatter.format(.init(value: 1, timescale: 1)) == "00:01")
         #expect(formatter.format(.init(value: 60, timescale: 1)) == "01:00")
         #expect(formatter.format(.init(value: 3600, timescale: 1)) == "60:00")
 
         formatter = PlayerTimeAdaptiveFormatStyle(duration: 3600)
+        #expect(formatter.format(.invalid).isEmpty)
+        #expect(formatter.format(.zero) == "00:00:00")
         #expect(formatter.format(.init(value: 1, timescale: 1)) == "00:00:01")
         #expect(formatter.format(.init(value: 60, timescale: 1)) == "00:01:00")
         #expect(formatter.format(.init(value: 3600, timescale: 1)) == "01:00:00")
