97758c274912208eb29354fb36f50a9fbe30eb01
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-08-06 18:11:27 +0200

    Start at the right time on the local player

--- Demo/Sources/Player/PlaylistEntry.swift
+++ Demo/Sources/Player/PlaylistEntry.swift
@@ -4,14 +4,15 @@
 //  License information is available from the LICENSE file.
 //
 
+import CoreMedia
 import PillarboxPlayer
 
 struct PlaylistEntry: Hashable {
     let media: Media
     let item: PlayerItem
 
-    init(media: Media) {
+    init(media: Media, startTime: @autoclosure @escaping () -> CMTime = .zero) {
         self.media = media
-        self.item = media.playerItem()
+        self.item = media.playerItem(with: .init(position: at(startTime())))
     }
 }
