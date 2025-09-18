064840fd2a6e030c28c20d912768dd5ef98bc3ee
Samuel Défago <defagos@gmail.com>
2025-09-18 14:34:31 +0200

    Add mini player view accessibility labels

--- Sources/Castor/UserInterface/CastMiniPlayerView.swift
+++ Sources/Castor/UserInterface/CastMiniPlayerView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 
 private struct _CastMiniPlayerView: View {
     @ObservedObject var player: CastPlayer
-    let cast: Cast
+    @ObservedObject var cast: Cast
 
     var body: some View {
         if ![.idle, .unknown].contains(player.state) {
@@ -31,11 +31,23 @@ private struct _CastMiniPlayerView: View {
             regularInfoView(for: asset)
             compactInfoView(for: asset)
         }
+        .accessibilityElement()
+        .accessibilityLabel(accessibilityLabel)
     }
 
     private func playbackButton() -> some View {
         PlaybackButton(player: player)
             .font(.system(size: 40))
+    }
+}
+
+private extension _CastMiniPlayerView {
+    var accessibilityLabel: String {
+        var label = name(for: player.currentAsset)
+        if let deviceName = cast.currentDevice?.name {
+            label.append(", \(route(to: deviceName))")
+        }
+        return label
     }
 }
 
@@ -69,8 +81,16 @@ private extension _CastMiniPlayerView {
             .minimumScaleFactor(0.7)
     }
 
+    private func name(for asset: CastAsset?) -> String {
+        asset?.metadata?.title ?? String(localized: "Unknown", bundle: .module, comment: "Generic name for a Cast device")
+    }
+
+    private func route(to deviceName: String) -> String {
+        String(localized: "Casting on \(deviceName)", bundle: .module, comment: "Current Cast receiver (with device name as wildcard)")
+    }
+
     private func title(for asset: CastAsset?) -> some View {
-        Text(asset?.metadata?.title ?? "Untitled")
+        Text(name(for: asset))
             .font(.subheadline)
             .bold()
             .lineLimit(1)
@@ -79,7 +99,7 @@ private extension _CastMiniPlayerView {
     @ViewBuilder
     private func subtitle() -> some View {
         if let deviceName = cast.currentDevice?.name {
-            Text("Casting on \(deviceName)", bundle: .module, comment: "Current Cast receiver (with device name as wildcard)")
+            Text(route(to: deviceName))
                 .foregroundStyle(.secondary)
                 .font(.subheadline)
                 .lineLimit(1)
