0e1fb4607859c2554e9f0a0c73a20e0215212209
Samuel Défago <defagos@gmail.com>
2025-10-09 15:35:44 +0200

    Add multizone devices recipe

---
+++ Sources/Castor/Recipes/MultizoneDevicesRecipe.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+
+final class MultizoneDevicesRecipe: NSObject, ReceiverStateRecipe {
+    static var defaultValue: [CastMultizoneDevice] = []
+
+    private let update: ([CastMultizoneDevice]) -> Void
+
+    private var devices: [CastMultizoneDevice] = [] {
+        didSet {
+            update(devices)
+        }
+    }
+
+    init(service: GCKCastSession, update: @escaping ([CastMultizoneDevice]) -> Void) {
+        self.update = update
+        super.init()
+        service.add(self)
+    }
+
+    static func status(from service: GCKCastSession) -> [CastMultizoneDevice] {
+        []
+    }
+}
+
+extension MultizoneDevicesRecipe: @preconcurrency GCKCastDeviceStatusListener {
+    func castSession(_ castSession: GCKCastSession, didReceive multizoneStatus: GCKMultizoneStatus) {
+        devices = multizoneStatus.devices.map { $0.toCastDevice() }
+    }
+}
