694df22222b3691b081133f5775958f7b91103cf
Samuel Défago <defagos@gmail.com>
2025-10-09 15:23:45 +0200

    Add multizone device

---
+++ Sources/Castor/Cast/CastMultizoneDevice.swift
@@ -0,0 +1,38 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+
+/// An object that represents a multizone Cast receiver device.
+public struct CastMultizoneDevice: Hashable {
+    let rawDevice: GCKMultizoneDevice
+
+    /// The device's friendly name.
+    public var name: String? {
+        rawDevice.friendlyName
+    }
+
+    fileprivate init(from device: GCKMultizoneDevice) {
+        self.rawDevice = device
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.rawDevice.deviceID == rhs.rawDevice.deviceID
+    }
+}
+
+extension CastMultizoneDevice {
+    static func name(for device: CastMultizoneDevice?) -> String {
+        device?.name ?? String(localized: "Unknown", bundle: .module, comment: "Generic name for a Cast device")
+    }
+}
+
+extension GCKMultizoneDevice {
+    func toCastDevice() -> CastMultizoneDevice {
+        .init(from: self)
+    }
+}
