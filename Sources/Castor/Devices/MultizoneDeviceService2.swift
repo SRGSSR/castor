7cab70b1e58997f4b07fba69292d70568dfefa4d
Samuel Défago <defagos@gmail.com>
2025-10-23 09:41:31 +0200

    Prepare new standalone volume and muted recipes

---
+++ Sources/Castor/Devices/MultizoneDeviceService2.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import GoogleCast
+
+struct MultizoneDeviceService2 {
+    let device: GCKMultizoneDevice
+    let sessionManager: GCKSessionManager
+}
