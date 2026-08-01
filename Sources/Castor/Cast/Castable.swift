beb93a8b21cca5a903f94185d2d6998a49dcc782
Samuel Défago <defagos@gmail.com>
2026-08-01 08:31:27 +0200

    Loosen main actor requirements

--- Sources/Castor/Cast/Castable.swift
+++ Sources/Castor/Cast/Castable.swift
@@ -5,15 +5,16 @@
 //
 
 /// A protocol that defines a castable context.
-@MainActor
 public protocol Castable: AnyObject {
     /// Invoked when a Cast session has been established.
     ///
     /// - Returns: The current state.
+    @MainActor
     func castStartSession() -> CastResumeState?
 
     /// Invoked when the Cast session is about to stop.
     ///
     /// - Parameter state: The state immediately before the Cast session ends.
+    @MainActor
     func castEndSession(with state: CastResumeState?)
 }
