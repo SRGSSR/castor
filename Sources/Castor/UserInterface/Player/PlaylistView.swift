c0e93f96824e5ba7611c17d249a0dfb503690576
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-09-25 11:22:41 +0200

    Fix PlaylistView selection

--- Sources/Castor/UserInterface/Player/PlaylistView.swift
+++ Sources/Castor/UserInterface/Player/PlaylistView.swift
@@ -20,7 +20,7 @@ struct PlaylistView: View {
     }
 
     private func list() -> some View {
-        List($player.items, editActions: .all, selection: $player.currentItem) { $item in
+        List($player.items, id: \.self, editActions: .all, selection: $player.currentItem) { $item in
             ItemCell(item: item)
         }
         .listStyle(.plain)
